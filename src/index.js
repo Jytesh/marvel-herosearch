5e2c4c6762ddbacf7679b5308c0ba0f4003a5450
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-03-10 11:51:00 +0530

    Doc: Updated readme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import marvelAPI from './api';
 
 import CharacterCarousel from './Components/Character';
+import GithubBadge from 'react-github-corner'
+
+
 class Input extends React.Component {
   constructor(){
     super()
@@ -42,3 +45,7 @@ class Input extends React.Component {
   }
 }
 ReactDOM.render(<Input />, document.getElementById('root'));
+ReactDOM.render((
+  <GithubBadge 
+  title= "View the repo on GitHub"
+  href="https://github.com/Jytesh/marvel-herosearch"/>), document.getElementById("badge"))
